125643d3242f4a042b96c4f07941311b288ca6a9
Adnan Issadeen <eddyrox1@hotmail.com>
2015-10-16 08:43:52 +0530

    Added updating of schedule. Addresses #8
    Schedules are now updated. Timezones can be changed too

--- src/core/scheduler/Scheduler.py
+++ src/core/scheduler/Scheduler.py
@@ -3,10 +3,7 @@ from datetime import datetime
 
 from crontab import CronTab
 def scheduleReply(replyId, dateAndTime):
-    config = configparser.ConfigParser()
-    config.read('src/config.ini')
-    pythonPath = config['CONFIG']['pythonPath']
-    dataStrategyInitializer = config['CONFIG']['dataStrategyInitializer']
+    pythonPath, dataStrategyInitializer = getConfig()
     command = '/bin/sh /replylater/src/core/runmessage.sh --id=%s --data=%s'%(str(replyId), dataStrategyInitializer)
     timeToPost = datetime.fromtimestamp(dateAndTime.timestamp())
     cron = CronTab(user=True)
@@ -16,3 +13,24 @@ def scheduleReply(replyId, dateAndTime):
     job.day.on(timeToPost.day)
     job.month.on(timeToPost.month)
     cron.write()
+
+def updateReply(replyId, dateAndTime):
+    pythonPath, dataStrategyInitializer = getConfig()
+    command = '/bin/sh /replylater/src/core/runmessage.sh --id=%s --data=%s'%(str(replyId), dataStrategyInitializer)
+    timeToPost = datetime.fromtimestamp(dateAndTime.timestamp())
+    cron = CronTab(user=True)
+    job = cron.remove_all(comment=str(replyId))
+    job = cron.new(command=command, comment=str(replyId))
+    job.minute.on(timeToPost.minute)
+    job.hour.on(timeToPost.hour)
+    job.day.on(timeToPost.day)
+    job.month.on(timeToPost.month)
+    cron.write()
+
+
+def getConfig():
+    config = configparser.ConfigParser()
+    config.read('src/config.ini')
+    pythonPath = config['CONFIG']['pythonPath']
+    dataStrategyInitializer = config['CONFIG']['dataStrategyInitializer']
+    return pythonPath, dataStrategyInitializer
