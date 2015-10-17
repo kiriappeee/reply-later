3679e0cf902a19bf6b359e742176e7ff4fa9a595
Adnan Issadeen <eddyrox1@hotmail.com>
2015-10-17 09:17:45 +0530

    Implemented the login flow. Finally! Closes #15
    Although this doesn't complete the user creation flow, the main piece
    which is authenticating the user and getting the auth tokens is
    complete.
    
    The login controller should ideally transfer the tweepy calls to the
    TweetAdapter but since this is authentication specific functionality and
    therefore used only ONCE through the application, it doesnt have to be
    there. Will move it into the Tweet Adapter eventually.

--- src/views.py
+++ src/views.py
@@ -1,8 +1,20 @@
 from flask import session,url_for,request,redirect,render_template
 from . import application
+from .controllers import LoginController
 
 BASEPATH = "/replylater/app"
 
 @application.route(BASEPATH + '/login', methods=['GET', 'POST'])
 def login():
-    return 'Hello world'
+    if request.method == 'GET':
+        return render_template('login.html')
+    elif request.method == 'POST':
+        return redirect(LoginController.getStartingUrl())
+
+@application.route(BASEPATH + '/completelogin', methods=['GET'])
+def completelogin():
+    if 'denied' in request.args:
+        return "DENIEEED"
+    else:
+        accessToken, accessTokenSecret = LoginController.getAuth(request.args.get('oauth_verifier'), request.args.get('oauth_token'))
+        return "verified"
