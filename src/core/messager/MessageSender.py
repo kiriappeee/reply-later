ed33e083a3f18d111de691e41bf9152972c6f0a4
Adnan Issadeen <eddyrox1@hotmail.com>
2015-10-18 10:01:23 +0530

    Shell script now runs with the proper command via cron.
    runmessage.sh: it now runs MessageSender with the correct parameters
    
    MessageSender.py: Can now accept parameters and runs the main method
    that parses the arguments and runs the actual method to send the message
    
    DataConfig.py: Edited initializer to take a string like "sqllite" and
    configure the data strategy based on that. I think I'm reinventing some
    factory pattern here

--- src/core/messager/MessageSender.py
+++ src/core/messager/MessageSender.py
@@ -3,10 +3,13 @@ from . import MessageBreaker
 from ..scheduler import Scheduler
 from ..reply import ReplyCRUD
 from ..data import DataConfig
+import argparse
 
 def sendMessage(replyId, dataStrategyInitializer):
     DataConfig.initializeDataStrategy(dataStrategyInitializer)
     reply = ReplyCRUD.getReplyByReplyId(replyId, DataConfig.ReplyDataStrategy)
+    print(reply.sentStatus)
+    print(DataConfig.ReplyDataStrategy)
     messagesToSend = MessageBreaker.breakMessage(reply.message, reply.tweetId, reply.userId, DataConfig.UserDataStrategy)
     tweetIdList = []
     tweetId = reply.tweetId
@@ -14,6 +17,18 @@ def sendMessage(replyId, dataStrategyInitializer):
         tweetId = TweetAdapter.sendReply(message, tweetId, reply.userId, DataConfig.UserDataStrategy)
         tweetIdList.append(tweetId)
     reply.sentStatus = "sent"
-    ReplyCRUD.updateReply(reply, DataConfig.ReplyDataStrategy)
+    print(reply.sentStatus)
+    print(ReplyCRUD.updateReply(reply, DataConfig.ReplyDataStrategy))
     return {"result": "success", "value":{"tweets": tweetIdList} }
+
+
+if __name__ == "__main__":
+    f = open('executed', 'w')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data')
+    parser.add_argument('--id')
+    args = parser.parse_args()
+    f.write(args.id + ' ' + args.data)
+    f.close()
+    sendMessage(args.id, args.data)
 
