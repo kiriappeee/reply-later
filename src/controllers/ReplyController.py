afca490eb8c94e8961648c93e9dc5702c01836a3
Adnan Issadeen <eddyrox1@hotmail.com>
2015-10-22 08:24:44 +0530

    Completed interface to reply to tweet of choice. Addresses #27
    ReplyController.py:
    
    * Changed how the tweet id would be sent to the reply
    controller since the id request arg can now be either a tweet id or a
    full tweet url too. Therefore the id is now part of the form that is
    sent.
    * Defined a method to hook into the core to get the tweet by url or id
    
    TestReplyController.py: Updated to match changes above
    
    createreply.html:
    
    * Changed form to carry the tweet id in a hidden input
    * Prefills the username now in the reply box
    
    replyscheduler.html:
    
    * Added a text input to allow user to paste a tweet url/id into
    
    views.py:
    
    * Changed the createReply call to now pass in only the form (id argument
      was removed)
    * Added a call to getTweet so result is passed into template. Need to
      add error handling

--- src/controllers/ReplyController.py
+++ src/controllers/ReplyController.py
@@ -8,8 +8,12 @@ from ..core.reply import ReplyCRUD
 def getMentions(userId):
     return TweetAdapter.getMentions(userId, 40, DataConfig.UserDataStrategy)
 
-def createReply(replyForm, tweetId, userId):
+def createReply(replyForm, userId):
     timeZone = timezone(timedelta(hours=int(replyForm['tzhour']), minutes=int(replyForm['tzminute'])))
     scheduledTime = datetime(datetime.now().year, int(replyForm['month']), int(replyForm['day']), int(replyForm['hour']), int(replyForm['minute']), tzinfo = timeZone)
-    replyToSave = Reply(userId, replyForm['message'], scheduledTime, timeZone, tweetId)
+    replyToSave = Reply(userId, replyForm['message'], scheduledTime, timeZone, replyForm['tweetId'])
     return ReplyCRUD.saveReply(replyToSave, DataConfig.ReplyDataStrategy)
+
+def getTweet(tweetIdOrUrl, userId):
+    tweet = TweetAdapter.getSingleTweet(tweetIdOrUrl, userId, DataConfig.UserDataStrategy)
+    return tweet
