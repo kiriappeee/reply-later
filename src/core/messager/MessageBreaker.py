7640fa1965c927f559c5dcef75d3253bfbf6dd62
Adnan Issadeen <eddyrox1@hotmail.com>
2015-10-26 15:40:03 +0530

    Added support for shortened links to message breaking
    This needs to be refactored and tested more before being finalised. Also
    need to decide on a way to store the Tweet url lengths

--- src/core/messager/MessageBreaker.py
+++ src/core/messager/MessageBreaker.py
@@ -1,15 +1,29 @@
 from . import TweetAdapter
+from ttp import ttp
 def breakMessage(messageToBreak, tweetId, userId, userDataStrategy):
     if len(messageToBreak) <= 140:
         return [ messageToBreak ]
     username = TweetAdapter.getUsernameForTweet(tweetId, userId, userDataStrategy)
     messagesToSend = []
+    urlLength, urlLengthHttps = TweetAdapter.getUrlLengths()
+    p = ttp.Parser()
+    parsed = p.parse(messageToBreak)
+    urls = parsed.urls
     while messageToBreak!="":
         if len(messageToBreak) > 140:
-            if messageToBreak[140] != " ":
-                cutOffIndex = messageToBreak[0:140].rfind(" ") + 1
+            endIndexToSearch = 140
+            for url in urls:
+                if messageToBreak.find(url) != -1:
+                    if url.startswith('https://'):
+                        if messageToBreak.find(url) < endIndexToSearch - urlLengthHttps:
+                            endIndexToSearch = endIndexToSearch + urlLengthHttps
+                    else:
+                        if messageToBreak.find(url) < endIndexToSearch - urlLength:
+                            endIndexToSearch = endIndexToSearch + urlLength
+            if messageToBreak[endIndexToSearch] != " ":
+                cutOffIndex = messageToBreak[0:endIndexToSearch + 1].rfind(" ")
             else:
-                cutOffIndex = 140
+                cutOffIndex = endIndexToSearch
             messagesToSend.append(messageToBreak[:cutOffIndex].rstrip())
             
             messageToBreak = "@%s %s"%(username, messageToBreak[cutOffIndex:].lstrip())
